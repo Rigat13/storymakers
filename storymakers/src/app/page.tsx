aa432fa23f7aed5d41ae840a5ed12caccf9890b8
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-03-25 18:50:00 +0100

    Added brand reel and fixed modularity calls to cards
    Image is set at classname (at page.tsx) instead of the globals.css, so a single style can be used with multiple images.

--- storymakers/src/app/page.tsx
+++ storymakers/src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
         <Layout>
             <main className="flex flex-col items-center justify-center min-h-screen p-24">
                 <div className="flex flex-row">
+                    {/* |||||||||||||||||||||||||||||||||| LOGOS/BOTONS |||||||||||||||||||||||||||||||||| */}
                     <div className="w-1/2 p-4 mr-20">
                         <img src="logo-storymakers.png" alt="Logotip - Storymakers" className="mb-10"/>
                         <img src="tagline-ca.png" alt="Logotip - Storymakers" className="mb-10"/>
@@ -20,28 +21,52 @@ export default function Home() {
                             </a>
                         </div>
                     </div>
+                    {/* |||||||||||||||||||||||||||||||||| GALERIA |||||||||||||||||||||||||||||||||| */}
                     <div className="w-1/2 p-4 ml-20">
                         <img src="gallery-base.png" alt="Gallery base"/>
                     </div>
                 </div>
+                {/* |||||||||||||||||||||||||||||||||| REPTES |||||||||||||||||||||||||||||||||| */}
                 <h1 className="text-6xl font-bold mt-20 mb-10 text-[#FF4A00]">Reptes</h1>
-                <div className="banner-container">
+                <div className="banner-container bg-[url('../../public/orange-reel.png')]">
                     <div className="flip-card">
                         <div className="flip-card-inner">
-                            <div className="flip-card-front"></div>
-                            <div className="flip-card-back"></div>
+                            <div className="flip-card-front bg-[url('../../public/tedx2024-challenge.png')]"></div>
+                            <div className="flip-card-back bg-[url('../../public/tedx2024-challenge-rewards.png')]"></div>
                         </div>
                     </div>
                     <div className="flip-card">
                         <div className="flip-card-inner">
-                            <div className="flip-card-front"></div>
-                            <div className="flip-card-back"></div>
+                            <div className="flip-card-front bg-[url('../../public/tedx2024-challenge.png')]"></div>
+                            <div className="flip-card-back bg-[url('../../public/tedx2024-challenge-rewards.png')]"></div>
                         </div>
                     </div>
                     <div className="flip-card">
                         <div className="flip-card-inner">
-                            <div className="flip-card-front"></div>
-                            <div className="flip-card-back"></div>
+                            <div className="flip-card-front bg-[url('../../public/tedx2024-challenge.png')]"></div>
+                            <div className="flip-card-back bg-[url('../../public/tedx2024-challenge-rewards.png')]"></div>
+                        </div>
+                    </div>
+                </div>
+                {/* |||||||||||||||||||||||||||||||||| MARQUES |||||||||||||||||||||||||||||||||| */}
+                <h1 className="text-6xl font-bold mt-20 mb-10 text-[#0035FF]">Marques</h1>
+                <div className="banner-container bg-[url('../../public/blue-reel.png')]">
+                    <div className="flip-card">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front bg-[url('../../public/tedx-business.png')]"></div>
+                            <div className="flip-card-back bg-[url('../../public/tedx2024-challenge-rewards.png')]"></div>
+                        </div>
+                    </div>
+                    <div className="flip-card">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front bg-[url('../../public/tedx-business.png')]"></div>
+                            <div className="flip-card-back bg-[url('../../public/tedx2024-challenge-rewards.png')]"></div>
+                        </div>
+                    </div>
+                    <div className="flip-card">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front bg-[url('../../public/tedx-business.png')]"></div>
+                            <div className="flip-card-back bg-[url('../../public/tedx2024-challenge-rewards.png')]"></div>
                         </div>
                     </div>
                 </div>
