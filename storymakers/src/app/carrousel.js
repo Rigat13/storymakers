9345070631fb9a4faa0fa3b4fa15907070808a8c
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-03-26 16:03:16 +0100

    Carrousel - 2 | Made carrousel auto rotate
    Also removed logs

--- storymakers/src/app/carrousel.js
+++ storymakers/src/app/carrousel.js
@@ -1,34 +1,47 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
+
+let isCooldown = false;
+let autoCarrouselDelay = 3000;
+let elems;
+
+async function autoCarrousel() {
+    while (true) {
+        if (!isCooldown) {
+            const next = elems.find((elem) => elem.dataset.pos == 1);
+            next.click();
+            isCooldown = true;
+            await new Promise(resolve => setTimeout(resolve, autoCarrouselDelay)); // Wait for 2 secondss
+            isCooldown = false;
+        }
+        await new Promise(resolve => setTimeout(resolve, 0));
+    }
+}
 
 export default function Carrousel() {
     let carouselListRef = useRef(null);
-    let carouselItemsRefs = Array.from({ length: 5 }, () => useRef(null));
+    let carouselItemsRefs = Array.from({length: 5}, () => useRef(null));
 
     useEffect(() => {
         function setupCarrousel() {
-            console.log("setupCarousel called!");
             const carouselList = carouselListRef.current;
             const carouselItems = carouselItemsRefs.map(ref => ref.current);
 
             if (!carouselList) return; // Ensure the carousel list is defined
 
             carouselList.addEventListener('click', function (event) {
-                console.log('ENTERED');
                 var newActive = event.target;
                 var isItem = newActive.closest('.carousel__item');
 
                 if (!isItem || newActive.classList.contains('carousel__item_active')) {
                     return;
                 }
-
                 update(newActive);
             });
 
-            const elems = carouselItems;
+            elems = carouselItems;
 
             const update = function (newActive) {
-                console.log('UPDATED');
                 const newActivePos = newActive.dataset.pos;
 
                 const current = elems.find((elem) => elem.dataset.pos == 0);
@@ -56,8 +69,8 @@ export default function Carrousel() {
                 return diff;
             };
         }
-
         setupCarrousel();
+        autoCarrousel();
     }, []);
 
     return (
