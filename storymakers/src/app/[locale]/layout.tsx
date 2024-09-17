fc625d3a116e514dc5a6536845cc186be66f46bf
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-09-17 15:46:24 +0200

    Update layout.tsx

--- storymakers/src/app/[locale]/layout.tsx
+++ storymakers/src/app/[locale]/layout.tsx
@@ -47,9 +47,9 @@ export default async function RootLayout({
 
   const pages = [
     {"tab":"about", "href":"es/about.html","color":'black'},
-     {"tab":"retos", "href":"es/retos.html","color":'black'},
-    {"tab":"blog", "href":"es/blog.html","color":'black'},
-    {"tab":"contacto", "href":"es/contact.html","color":'black'}];
+    {"tab":"retos", "href":"es/retos.html","color":'black'},
+    {"tab":"blog", "href":"https://www.linkedin.com/company/stoymakers-es/posts/?feedView=all","color":'black'},
+    {"tab":"contacto", "href":"https://tally.so/r/mBG4E1","color":'black'}];
 
 
     return (
